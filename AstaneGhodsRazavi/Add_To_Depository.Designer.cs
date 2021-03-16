d30730fbbc95f80821d7788252772bda5af45c63
farshad <farshadnp6@gmail.com>
2021-03-16 23:43:48 +0700

    add data source

--- AstaneGhodsRazavi/Add_To_Depository.Designer.cs
+++ AstaneGhodsRazavi/Add_To_Depository.Designer.cs
@@ -29,13 +29,52 @@ namespace AstaneGhodsRazavi
         /// </summary>
         private void InitializeComponent()
         {
+            this.TP_Tottal = new System.Windows.Forms.TabPage();
+            this.TP_Clothing = new System.Windows.Forms.TabPage();
+            this.TP_Food = new System.Windows.Forms.TabPage();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.TP_AddToDepository = new System.Windows.Forms.TabPage();
-            this.TP_Food = new System.Windows.Forms.TabPage();
-            this.TP_Clothing = new System.Windows.Forms.TabPage();
-            this.TP_Tottal = new System.Windows.Forms.TabPage();
+            this.dockPanel1 = new DevExpress.XtraBars.Docking.DockPanel();
+            this.fieldListDockPanel1_Container = new DevExpress.XtraReports.UserDesigner.DesignControlContainer();
+            this.reportExplorerDockPanel1_Container = new DevExpress.XtraReports.UserDesigner.DesignControlContainer();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TP_Tottal
+            // 
+            this.TP_Tottal.BackColor = System.Drawing.Color.Transparent;
+            this.TP_Tottal.Font = new System.Drawing.Font("IRAN Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.TP_Tottal.ForeColor = System.Drawing.Color.DarkRed;
+            this.TP_Tottal.Location = new System.Drawing.Point(4, 29);
+            this.TP_Tottal.Name = "TP_Tottal";
+            this.TP_Tottal.Padding = new System.Windows.Forms.Padding(3);
+            this.TP_Tottal.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.TP_Tottal.Size = new System.Drawing.Size(798, 337);
+            this.TP_Tottal.TabIndex = 0;
+            this.TP_Tottal.Text = "کل موجودی";
+            // 
+            // TP_Clothing
+            // 
+            this.TP_Clothing.BackColor = System.Drawing.Color.Transparent;
+            this.TP_Clothing.Location = new System.Drawing.Point(4, 29);
+            this.TP_Clothing.Name = "TP_Clothing";
+            this.TP_Clothing.Padding = new System.Windows.Forms.Padding(3);
+            this.TP_Clothing.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.TP_Clothing.Size = new System.Drawing.Size(798, 337);
+            this.TP_Clothing.TabIndex = 2;
+            this.TP_Clothing.Text = "پوشاک";
+            // 
+            // TP_Food
+            // 
+            this.TP_Food.BackColor = System.Drawing.Color.Transparent;
+            this.TP_Food.Location = new System.Drawing.Point(4, 29);
+            this.TP_Food.Name = "TP_Food";
+            this.TP_Food.Padding = new System.Windows.Forms.Padding(3);
+            this.TP_Food.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.TP_Food.Size = new System.Drawing.Size(798, 337);
+            this.TP_Food.TabIndex = 1;
+            this.TP_Food.Text = "خوراکی";
+            this.TP_Food.Click += new System.EventHandler(this.TP_Food_Click);
             // 
             // tabControl1
             // 
@@ -61,41 +100,29 @@ namespace AstaneGhodsRazavi
             this.TP_AddToDepository.Text = "اضافه کردن به انبار";
             this.TP_AddToDepository.Click += new System.EventHandler(this.TP_AddToDepository_Click);
             // 
-            // TP_Food
+            // dockPanel1
             // 
-            this.TP_Food.BackColor = System.Drawing.Color.Transparent;
-            this.TP_Food.Location = new System.Drawing.Point(4, 29);
-            this.TP_Food.Name = "TP_Food";
-            this.TP_Food.Padding = new System.Windows.Forms.Padding(3);
-            this.TP_Food.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.TP_Food.Size = new System.Drawing.Size(793, 337);
-            this.TP_Food.TabIndex = 1;
-            this.TP_Food.Text = "خوراکی";
-            this.TP_Food.Click += new System.EventHandler(this.TP_Food_Click);
+            this.dockPanel1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Fill;
+            this.dockPanel1.ID = new System.Guid("6865fb54-aeda-4a74-ad7c-8eba42ee95c6");
+            this.dockPanel1.Location = new System.Drawing.Point(0, 0);
+            this.dockPanel1.Name = "dockPanel1";
+            this.dockPanel1.OriginalSize = new System.Drawing.Size(374, 251);
+            this.dockPanel1.Size = new System.Drawing.Size(0, 0);
+            this.dockPanel1.Text = "panelContainer2";
             // 
-            // TP_Clothing
+            // fieldListDockPanel1_Container
             // 
-            this.TP_Clothing.BackColor = System.Drawing.Color.Transparent;
-            this.TP_Clothing.Location = new System.Drawing.Point(4, 29);
-            this.TP_Clothing.Name = "TP_Clothing";
-            this.TP_Clothing.Padding = new System.Windows.Forms.Padding(3);
-            this.TP_Clothing.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.TP_Clothing.Size = new System.Drawing.Size(793, 337);
-            this.TP_Clothing.TabIndex = 2;
-            this.TP_Clothing.Text = "پوشاک";
+            this.fieldListDockPanel1_Container.Location = new System.Drawing.Point(1, 23);
+            this.fieldListDockPanel1_Container.Name = "fieldListDockPanel1_Container";
+            this.fieldListDockPanel1_Container.Size = new System.Drawing.Size(374, 371);
+            this.fieldListDockPanel1_Container.TabIndex = 0;
             // 
-            // TP_Tottal
+            // reportExplorerDockPanel1_Container
             // 
-            this.TP_Tottal.BackColor = System.Drawing.Color.Transparent;
-            this.TP_Tottal.Font = new System.Drawing.Font("IRAN Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.TP_Tottal.ForeColor = System.Drawing.Color.DarkRed;
-            this.TP_Tottal.Location = new System.Drawing.Point(4, 29);
-            this.TP_Tottal.Name = "TP_Tottal";
-            this.TP_Tottal.Padding = new System.Windows.Forms.Padding(3);
-            this.TP_Tottal.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.TP_Tottal.Size = new System.Drawing.Size(793, 337);
-            this.TP_Tottal.TabIndex = 0;
-            this.TP_Tottal.Text = "کل موجودی";
+            this.reportExplorerDockPanel1_Container.Location = new System.Drawing.Point(0, 0);
+            this.reportExplorerDockPanel1_Container.Name = "reportExplorerDockPanel1_Container";
+            this.reportExplorerDockPanel1_Container.Size = new System.Drawing.Size(0, 0);
+            this.reportExplorerDockPanel1_Container.TabIndex = 0;
             // 
             // Add_To_Depository
             // 
@@ -115,11 +142,13 @@ namespace AstaneGhodsRazavi
         }
 
         #endregion
-
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage TP_Tottal;
+        private System.Windows.Forms.TabPage TP_AddToDepository;
         private System.Windows.Forms.TabPage TP_Food;
         private System.Windows.Forms.TabPage TP_Clothing;
-        private System.Windows.Forms.TabPage TP_AddToDepository;
+        private System.Windows.Forms.TabPage TP_Tottal;
+        private DevExpress.XtraBars.Docking.DockPanel dockPanel1;
+        private DevExpress.XtraReports.UserDesigner.DesignControlContainer fieldListDockPanel1_Container;
+        private DevExpress.XtraReports.UserDesigner.DesignControlContainer reportExplorerDockPanel1_Container;
     }
 }
--- AstaneGhodsRazavi/Properties/Settings.Designer.cs
+++ AstaneGhodsRazavi/Properties/Settings.Designer.cs
@@ -33,5 +33,15 @@ namespace AstaneGhodsRazavi.Properties {
                 return ((string)(this["AstanDBConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=Astan;Integrated Security=True")]
+        public string AstanConnectionString {
+            get {
+                return ((string)(this["AstanConnectionString"]));
+            }
+        }
     }
 }
